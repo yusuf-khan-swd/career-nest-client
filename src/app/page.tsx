cec5c3f2a7133e82e245485940fdef960086a339
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 20:37:45 +0600

    add navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import HomePage from "@/components/Home/HomePage";
+import Navbar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <HomePage />
     </main>
   );
