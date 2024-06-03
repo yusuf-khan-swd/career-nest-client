4fbb4c8ddbfb8fad2bf344b0114f0734316e2af0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 22:12:20 +0600

    Add Navbar and Footer component to register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,13 @@
 import RegisterPage from "@/components/Logins/RegisterPage";
+import Footer from "@/components/ui/Footer";
+import Navbar from "@/components/ui/Navbar";
 
 const Register = () => {
   return (
     <div>
+      <Navbar />
       <RegisterPage />
+      <Footer />
     </div>
   );
 };
