4dda36d1aedec457f4086911138d6cc91820890f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 20:08:13 +0600

    create home component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,7 @@
+import Home from "@/components/Home/Home";
+
 const page = () => {
-  return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-      <h1 className="text-3xl font-bold text-center">Home page</h1>
-    </div>
-  );
+  return <Home />;
 };
 
 export default page;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Home page</h1>
+    </div>
+  );
+};
+
+export default Home;
