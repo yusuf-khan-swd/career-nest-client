03ba9d2d56ef0b30aa284136a46373221135c1c5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 22:11:38 +0600

    Add Navbar and Footer component to Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
 import LoginPage from "@/components/Logins/LoginPage";
+import Footer from "@/components/ui/Footer";
+import Navbar from "@/components/ui/Navbar";
 
 const Login = () => {
-  return <LoginPage />;
+  return (
+    <>
+      <Navbar />
+      <LoginPage />;
+      <Footer />
+    </>
+  );
 };
 
 export default Login;
