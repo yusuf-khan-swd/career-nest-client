5d50fc66005e322812b154dcb6b617e9021e7ee0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 21:18:38 +0600

    add navbar to mainlayout

---
+++ src/app/(mainlayout)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/ui/Navbar";
+import React from "react";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default MainLayout;
