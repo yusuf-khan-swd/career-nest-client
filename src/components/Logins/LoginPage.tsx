14a2c164a19cfcb88d1923cc7bb09f44e9ff09b2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 23:04:48 +0600

    Add a client LoginPage component with its form and input field

--- src/components/Logins/LoginPage.tsx
+++ src/components/Logins/LoginPage.tsx
@@ -1,8 +1,83 @@
+"use client";
+
+import Link from "next/link";
+import GoogleLogin from "./GoogleLogin";
+
 const LoginPage = () => {
+  // const { signIn, user } = useAuth();
+
+  const handleSUbmit = async (e: any) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email, password);
+
+    // await signIn(email, password);
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <form onSubmit={handleSUbmit} className="hero min-h-[80vh] bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse max-w-[1040px] mx-auto">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                name="email"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                name="password"
+                required
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <input
+                className="btn bg-red-500 text-white"
+                type="submit"
+                value="Login"
+              />
+            </div>
+            <div className="mt-6">
+              <GoogleLogin />
+            </div>
+            <div className="mt-6">
+              <p>
+                New here?{" "}
+                <Link href="/register" className="text-red-500">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   );
 };
 
