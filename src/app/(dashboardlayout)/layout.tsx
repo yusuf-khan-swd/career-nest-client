81dc7eb8936111ee48e48d5f7db085aaec8c7613
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 21:30:17 +0600

    create dashboard layout

---
+++ src/app/(dashboardlayout)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/ui/Navbar";
+import React from "react";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
