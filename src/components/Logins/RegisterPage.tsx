24a140ffa925b45891c8dec58ef637074b4a9831
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 23:00:27 +0600

    use form in after card body not in top

--- src/components/Logins/RegisterPage.tsx
+++ src/components/Logins/RegisterPage.tsx
@@ -45,7 +45,7 @@ const RegisterPage = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse max-w-[1040px] mx-auto">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Register now!</h1>
@@ -57,81 +57,83 @@ const RegisterPage = () => {
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="name"
-                  className="input input-bordered"
-                  name="name"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  name="email"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                  name="password"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Confirm Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="confirm password"
-                  className="input input-bordered"
-                  name="confirm_password"
-                  required
-                />
-              </div>
-              {!passMatch && (
-                <div className="my-2">
-                  <p className="text-red-500">Passwords do not match!</p>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="name"
+                    className="input input-bordered"
+                    name="name"
+                    required
+                  />
                 </div>
-              )}
-              <div className="form-control mt-6">
-                <input
-                  className="btn bg-red-500 text-white"
-                  type="submit"
-                  value="Register"
-                />
-              </div>
-              <div className="mt-6">
-                <GoogleLogin />
-              </div>
-              <div className="mt-6">
-                <p>
-                  Already have an account?{" "}
-                  <Link href="/login" className="text-red-500">
-                    Login
-                  </Link>
-                </p>
-              </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Confirm Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="confirm password"
+                    className="input input-bordered"
+                    name="confirm_password"
+                    required
+                  />
+                </div>
+                {!passMatch && (
+                  <div className="my-2">
+                    <p className="text-red-500">Passwords do not match!</p>
+                  </div>
+                )}
+                <div className="form-control mt-6">
+                  <input
+                    className="btn bg-red-500 text-white"
+                    type="submit"
+                    value="Register"
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>
-      </form>
+      </div>
+      <div className="mt-6">
+        <GoogleLogin />
+      </div>
+      <div className="mt-6">
+        <p>
+          Already have an account?{" "}
+          <Link href="/login" className="text-red-500">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
