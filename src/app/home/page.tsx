e1bf12800d1464775a7fa2568c7fef8b213764a7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-03 19:58:45 +0600

    add tailwind css to project

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,13 @@
 const page = () => {
   return (
     <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
       <h1 className="text-3xl font-bold text-center">Home page</h1>
     </div>
   );
