db99b9a3757db753cbef3f6896f41418d8dbf761
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-04 19:22:46 +0600

    add dashboard data form for user

--- src/components/Dashboard/DashboardPage.tsx
+++ src/components/Dashboard/DashboardPage.tsx
@@ -1,7 +1,81 @@
+"use client";
+
+import { getBaseUrl } from "@/helpers/getBaseUrl";
+import useAuth from "@/hooks/useAuth";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 const DashboardPage = () => {
+  const { logout, user } = useAuth();
+
+  const [userInfo, setUserInfo] = useState<null | any>({});
+
+  useEffect(() => {
+    const baseUrl = getBaseUrl();
+    fetch(`${baseUrl}/users?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result?.data) {
+          setUserInfo(result?.data);
+        } else {
+          toast.error("User data failed to get");
+        }
+      });
+  }, [user]);
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
   return (
     <div>
-      <h1>Dashboard Page</h1>
+      <div className="flex justify-between mb-4">
+        <h1 className="text-3xl font-semibold">Profile Information</h1>
+        <Link
+          href={`/dashboard/profile/edit/${userInfo?._id}`}
+          className="btn btn-info"
+        >
+          Edit Profile
+        </Link>
+      </div>
+      <div className="hero bg-base-200 p-8">
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+                value={userInfo?.name}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                value={userInfo?.email}
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button
+                onClick={handleLogout}
+                className="btn btn-error text-white"
+              >
+                Logout
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
