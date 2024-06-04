607eb94afd1ee260457221ac46e7d55f19d1b0d9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-04 18:51:22 +0600

    create banner component and use it home page

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,6 +1,9 @@
+import Banner from "../ui/Banner";
+
 const HomePage = () => {
   return (
     <div>
+      <Banner />
       <h1 className="text-3xl font-bold text-center">Home page</h1>
     </div>
   );
