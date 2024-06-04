4f151a277bab811b0c7f9df26803e3c6ba4e9add
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-04 19:24:53 +0600

    remove margin bottom from Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100 border-b-2 mb-6">
+    <div className="navbar bg-base-100 border-b-2">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
