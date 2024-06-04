395a3a961ed534938d88c0065ee8fbd7f07992df
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 00:05:54 +0600

    initialize DashboardAllJobsPage component

--- src/components/DashboardAllJobs/DashboardAllJobsPage.tsx
+++ src/components/DashboardAllJobs/DashboardAllJobsPage.tsx
@@ -1,9 +1,32 @@
 "use client";
+import { useEffect, useState } from "react";
+import SingleDashboardJob from "./SingleDashboardJob";
 
 const DashboardAllJobsPage = () => {
+  const [products, setProducts] = useState<null | any>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/jobs")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleDeleteProduct = (id: string) => {
+    setProducts(products.filter((product: any) => product._id !== id));
+  };
+
   return (
     <div>
-      <h1>Dashboard Jobs</h1>
+      <h1 className="text-5xl font-bold text-center">All Products</h1>
+      <div className="my-16 flex justify-center flex-wrap gap-4">
+        {products.map((shoe: any) => (
+          <SingleDashboardJob
+            key={shoe?.id}
+            job={shoe}
+            onDelete={handleDeleteProduct}
+          />
+        ))}
+      </div>
     </div>
   );
 };
