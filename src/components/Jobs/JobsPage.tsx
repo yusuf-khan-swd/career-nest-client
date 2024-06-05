40a420c532e82e39656b034fdf69b3594ea7ba18
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 22:55:50 +0600

    use another state to store prevJobs

--- src/components/Jobs/JobsPage.tsx
+++ src/components/Jobs/JobsPage.tsx
@@ -6,18 +6,21 @@ import JobCard from "./JobCard";
 
 const JobsPage = () => {
   const [jobs, setJobs] = useState<null | any>([]);
+  const [prevJobs, setPrevJobs] = useState<null | any>([]);
 
   useEffect(() => {
     const baseUrl = getBaseUrl();
 
     fetch(`${baseUrl}/jobs`)
       .then((res) => res.json())
-      .then((data) => setJobs(data));
+      .then((data) => {
+        setJobs(data);
+        setPrevJobs(data);
+      });
   }, []);
 
   // Search is case sensitive
   const handleSearch = (value: any) => {
-    const prevJobs = [...jobs];
     if (value) {
       const filteredJobs = jobs.filter((job: any) => job.title.includes(value));
       setJobs(filteredJobs);
@@ -29,7 +32,7 @@ const JobsPage = () => {
   return (
     <div className="m-2 container mx-auto">
       <h1 className="text-3xl font-bold text-center mb-2">All Jobs</h1>
-      <div className="form-control mb-3">
+      <div className="form-control mb-4">
         <label className="label">
           <span className="label-text">Search</span>
         </label>
