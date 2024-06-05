3208be768a57f8044dbf2cb7c7359e60391a69df
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 16:26:20 +0600

    get all jobs info

--- src/components/Jobs/JobsPage.tsx
+++ src/components/Jobs/JobsPage.tsx
@@ -1,4 +1,19 @@
+import { getBaseUrl } from "@/helpers/getBaseUrl";
+import { useEffect, useState } from "react";
+
 const JobsPage = () => {
+  const [jobs, setJobs] = useState<null | any>([]);
+
+  useEffect(() => {
+    const baseUrl = getBaseUrl();
+
+    fetch(`${baseUrl}/jobs`)
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-center">Jobs page</h1>
