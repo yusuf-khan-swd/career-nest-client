8938283a74f06322d29909e9148bd74aef92bdb1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 20:53:17 +0600

    add contact us page

---
+++ src/components/Contact/ContactUsPage.tsx
@@ -0,0 +1,9 @@
+const ContactUsPage = () => {
+  return (
+    <div className="min-h-[80vh]">
+      <h1 className="text-3xl font-bold text-center">Coming Soon...</h1>
+    </div>
+  );
+};
+
+export default ContactUsPage;
