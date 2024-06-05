78634123162087f8551574a10a157844f899ea66
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 22:29:03 +0600

    remove svg

--- src/components/ui/Features.tsx
+++ src/components/ui/Features.tsx
@@ -10,28 +10,6 @@ const Features = () => {
           </div>
           <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
             <span className="relative inline-block">
-              <svg
-                viewBox="0 0 52 24"
-                fill="currentColor"
-                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-              >
-                <defs>
-                  <pattern
-                    id="ea469ae8-e6ec-4aca-8875-fc402da4d16e"
-                    x="0"
-                    y="0"
-                    width=".135"
-                    height=".30"
-                  >
-                    <circle cx="1" cy="1" r=".7" />
-                  </pattern>
-                </defs>
-                <rect
-                  fill="url(#ea469ae8-e6ec-4aca-8875-fc402da4d16e)"
-                  width="52"
-                  height="24"
-                />
-              </svg>
               <span className="relative">The</span>
             </span>{" "}
             quick, brown fox jumps over a lazy dog
