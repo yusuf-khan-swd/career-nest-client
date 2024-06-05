0838a66b9e8d3ca81c30c31bf20343daa57b4cf6
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 20:38:48 +0600

    add margin

--- src/components/ui/Statics.tsx
+++ src/components/ui/Statics.tsx
@@ -1,6 +1,6 @@
 const Statics = () => {
   return (
-    <div className="shadow-lg border rounded-lg mt-8">
+    <div className="shadow-lg border rounded-lg mt-12">
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="grid grid-cols-2 row-gap-8 md:grid-cols-4">
           <div className="text-center md:border-r">
