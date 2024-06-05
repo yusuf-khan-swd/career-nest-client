776660c0cd0102151ab0b7db8c9b022c074e9f7f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 21:06:12 +0600

    add about us content

--- src/components/About/AboutPage.tsx
+++ src/components/About/AboutPage.tsx
@@ -1,7 +1,19 @@
 const AboutPage = () => {
   return (
-    <div className="min-h-[80vh]">
-      <h1 className="text-3xl font-bold text-center">Coming Soon...</h1>
+    <div className="min-h-[80vh] container mx-auto max-w-[750px] mt-8">
+      <div className="mb-5">
+        <h1 className="text-3xl font-bold mb-2">Welcome to Career Nest</h1>
+        <p style={{ lineHeight: "28px" }}>
+          Where we believe in turning dreams into reality. Our team of skilled
+          craftsmen is dedicated to transforming houses into personalized
+          sanctuaries. With a passion for precision and an eye for detail, we
+          take pride in creating spaces that reflect your unique style and
+          preferences. At Career Nest, your home is not just a structure;
+          it&apos;s a canvas waiting to be adorned with creativity and
+          expertise. Join us on a journey of craftsmanship and commitment, where
+          every project is a masterpiece in the making.
+        </p>
+      </div>
     </div>
   );
 };
