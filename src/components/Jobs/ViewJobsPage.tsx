69265ac4d8440f1372111b32fb55e1d3c57947a6
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 16:16:40 +0600

    Add ViewJobsPage card useEffect

--- src/components/Jobs/ViewJobsPage.tsx
+++ src/components/Jobs/ViewJobsPage.tsx
@@ -1,7 +1,41 @@
+"use client";
+
+import { getBaseUrl } from "@/helpers/getBaseUrl";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
 const ViewJobsPage = ({ id }: { id: string }) => {
+  const [jobInfo, setJobInfo] = useState<null | any>({});
+
+  const { title, company, location, type, salary, description } = jobInfo;
+
+  useEffect(() => {
+    const baseUrl = getBaseUrl();
+
+    fetch(`${baseUrl}/jobs/${id}`)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          setJobInfo(result);
+        } else {
+          toast.error("Job data failed to get");
+        }
+      });
+  }, [id]);
+
   return (
     <div>
-      <h1>Details Jobs Page {id}</h1>
+      <div className="card w-full bg-base-100 shadow-xl border">
+        <div className="card-body">
+          <h2 className="card-title">Job Title: {title}</h2>
+          <p>Company: {company}</p>
+          <p className="font-semibold">Location: {location}</p>
+          <p>Job Type: {type}</p>
+          <p className="font-semibold">Salary: {salary}</p>
+          <p>Description: {description}</p>
+          <div className="card-actions justify-end"></div>
+        </div>
+      </div>
     </div>
   );
 };
