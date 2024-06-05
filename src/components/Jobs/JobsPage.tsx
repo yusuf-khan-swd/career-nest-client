f8cd6d0bc912370ee7c9c2f5263f1c90a638de57
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-05 18:37:35 +0600

    add search input field with onChange handleSearch function

--- src/components/Jobs/JobsPage.tsx
+++ src/components/Jobs/JobsPage.tsx
@@ -15,9 +15,24 @@ const JobsPage = () => {
       .then((data) => setJobs(data));
   }, []);
 
+  const handleSearch = (value: any) => {
+    console.log(value);
+  };
+
   return (
     <div className="m-2 container mx-auto">
       <h1 className="text-3xl font-bold text-center mb-2">All Jobs</h1>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Search</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered"
+          onChange={(e: any) => handleSearch(e.target.value)}
+        />
+      </div>
       <div className="grid grid-cols-1 gap-4">
         {jobs.map((job: any) => (
           <JobCard key={job?.id} job={job} />
